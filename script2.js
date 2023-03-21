79693254f59508043eeae5e3d2d6057440316322
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2023-03-21 14:31:06 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,20 @@
+(async function () {
+    try {
+      const response = await fetch("https://embed2.vercel.app/api/handler");
+      const { html, css } = await response.json();
+
+      const widgetContainer = document.getElementById("container01");
+  
+      if (widgetContainer) {
+        widgetContainer.innerHTML = html;
+  
+        const style = document.createElement("style");
+        style.textContent = css;
+        document.head.appendChild(style);
+      } else {
+        console.error("Error: No container found for the widget.");
+      }
+    } catch (error) {
+      console.error("Error loading widget:", error);
+    }
+  })();
